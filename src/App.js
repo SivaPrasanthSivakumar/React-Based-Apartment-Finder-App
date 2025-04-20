2cc3fb8a56982e1bdf7bec8e2cc8aef2ddd0989a
Siva Prasanth Sivakumar <sivaprasanth@gmail.com>
2025-04-20 16:58:33 -0400

    front end update

--- src/App.js
+++ src/App.js
@@ -48,17 +48,6 @@ function HomePage() {
       </div>
       <p>RENTING VS BUYING (what's better?)</p>
       <p>By Mark Tilbury</p>
-      <div style={{ marginTop: "2rem" }}>
-        <iframe
-          width="560"
-          height="315"
-          src="https://www.youtube.com/embed/dQw4w9WgXcQ"
-          title="YouTube video player"
-          frameBorder="0"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-          allowFullScreen
-        ></iframe>
-      </div>
     </div>
   );
 }
